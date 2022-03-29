e460bbb7be9c32426d9d9d9e9cf5efbe8cffbaa7
ebenetce <ebenetce@host.docker.internal>
2022-03-29 12:09:57 +0100

    Added warning upon fail

--- tbx/+SBTi/+utils/updateCompaniesTakingAction.m
+++ tbx/+SBTi/+utils/updateCompaniesTakingAction.m
@@ -1,8 +1,13 @@
-function updateCompaniesTakingAction(nvp)
+function outfilename = updateCompaniesTakingAction(nvp)
 
 arguments
     nvp.path = SBTiroot;
     nvp.name = 'current-Companies-Taking-Action-191.xlsx'
 end
 
-websave(fullfile(nvp.path, "+SBTi/+configs/inputs", nvp.name), 'https://sciencebasedtargets.org/download/excel')
+try
+    outfilename = websave(fullfile(nvp.path, "+SBTi/+configs/inputs", nvp.name), 'https://sciencebasedtargets.org/download/excel');
+catch
+    warning('SBTi:utils:updateCompaniesTakingAction:websaveFailed','Unable to update companies taking action spreadsheet')
+    outfilename = '';
+end
